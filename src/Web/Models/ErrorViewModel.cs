289dcf6ea2be81792c1d433f30b225a9110b22ef
Viktor Svyatokha <ahydrax@gmail.com>
2020-10-22 01:02:09 +1000

    Add template

---
+++ src/Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KNFA.Bots.MTB.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
