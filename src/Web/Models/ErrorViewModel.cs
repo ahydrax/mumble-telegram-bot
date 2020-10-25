f6a801af039b89b61de28b5f27a081264603f82e
Viktor Svyatokha <ahydrax@gmail.com>
2020-10-25 12:17:03 +1000

    Remove unused model

--- src/Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace KNFA.Bots.MTB.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
