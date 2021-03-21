30ef128aa90eee51ef2ba2a70b66c279995c3b1d
Viktor Svyatokha <ahydrax@gmail.com>
2021-03-21 13:15:02 +1000

    Add api for fetching active users

---
+++ src/Web/Controllers/ApiController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using KNFA.Bots.MTB.Services.Mumble;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KNFA.Bots.MTB.Controllers
+{
+    [AllowAnonymous]
+    [ApiController]
+    [Route("api")]
+    public class ApiController : Controller
+    {
+        private readonly IMumbleInfo _mumbleInfo;
+
+        public ApiController(IMumbleInfo mumbleInfo)
+        {
+            _mumbleInfo = mumbleInfo ?? throw new ArgumentNullException(nameof(mumbleInfo));
+        }
+
+        [HttpGet("users")]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await _mumbleInfo.GetUsersAsync();
+            return Ok(users);
+        }
+    }
+}
